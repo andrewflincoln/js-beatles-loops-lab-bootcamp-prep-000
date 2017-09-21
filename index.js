56ba980bb9eee2ec0b6cd10bba678641a4272536
Andrew Lincoln <andrewjlincoln@gmail.com>
2017-09-21 23:25:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var plays = []
 
-  for (var i = 0; i === musicians.length; i++) {
+  for (i = 0; i < musicians.length; i++) {
     plays[i] = `${musicians[i]} plays ${instruments[i]}`
   }
 
